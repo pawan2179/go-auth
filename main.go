a642e97c222e9d84b0199fc8e3c155766fd9ac9f
ace <pawansingh4418@gmail.com>
2026-03-12 23:07:41 +0530

    added chi-router

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"AuthInGo/app"
+	config "AuthInGo/config/env"
 	"fmt"
 )
 
 func main() {
+	config.Load()
 	fmt.Println("Starting application")
 
-	config := app.NewConfig(":3001")
+	config := app.NewConfig()
 	app := app.NewApplication(config)
 	app.Run()
 }
